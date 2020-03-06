e7013826057ddfbee210e547b3ce8efe512066f4
Joao Vale <jpvale@gmail.com>
2020-03-06 23:17:34 +0000

    Configure apps

--- macbootpro.sh
+++ macbootpro.sh
@@ -61,3 +61,34 @@ defaults write com.apple.dock wvous-tr-modifier -int 0
 # bottom right screen corner - show Desktop
 defaults write com.apple.dock wvous-br-corner -int 4
 defaults write com.apple.dock wvous-br-modifier -int 0
+
+
+## Configure apps
+echo "> Configure Clipy"
+defaults write com.clipy-app.Clipy kCPYPrefShowIconInTheMenuKey -int 0
+defaults write com.clipy-app.Clipy menuItemsAreMarkedWithNumbers -int 0
+defaults write com.clipy-app.Clipy kCPYPrefNumberOfItemsPlaceInlineKey -int 20
+defaults write com.clipy-app.Clipy kCPYPrefNumberOfItemsPlaceInsideFolderKey -int 20
+defaults write com.clipy-app.Clipy loginItem -int 1
+
+echo "> Configure Rectangle"
+defaults write com.knollsoft.Rectangle launchOnLogin -int 1
+
+echo "> Configure iTerm"
+defaults write com.googlecode.iterm2 HideTab -int 0
+defaults write com.googlecode.iterm2 StatusBarPosition -int 1
+
+echo "> Configure VS Code"
+code --install-extension Shan.code-settings-sync
+VSCODE_SETTINGS="$HOME/Library/Application Support/Code/User/settings.json"
+if test -s "$VSCODE_SETTINGS"
+then
+    echo "VS Code settings file already exists, skipping"
+else
+    cat <<EOF > "$VSCODE_SETTINGS"
+    {
+        "sync.removeExtensions": false,
+        "sync.gist": "366789c5627379c6b831ecc4a624e4a4",
+    }
+EOF
+fi
